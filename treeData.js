93975729770ee298bcc9fde7bb49be95c15c04a8
Matt Waldron <mattwaldron@gmail.com>
2016-06-04 17:04:29 -0400

    Comment out promise.

--- treeData.js
+++ treeData.js
@@ -3,23 +3,20 @@ const data2015 = 'uvpi-gqnh';
 
 class TreeData {
 
-  getPoorHealthTreeData(){
-    let request = new XMLHttpRequest();
-    return new Promise(function(resolve, reject){
-      request.onload = function(){
-        if (this.status >= 200 && this.status < 400) {
-          resolve(JSON.parse(this.response));
-        } else {
-          reject(console.error('Error'));
-        }
-      }
-      request.open('GET', 'https://data.cityofnewyork.us/resource/uvpi-gqnh.json?$where=health = "Poor"', true);
-      request.send();
-    });
-  }
-
-
-
+  // getPoorHealthTreeData(){
+  //   let request = new XMLHttpRequest();
+  //   return new Promise(function(resolve, reject){
+  //     request.onload = function(){
+  //       if (this.status >= 200 && this.status < 400) {
+  //         resolve(JSON.parse(this.response));
+  //       } else {
+  //         reject(console.error('Error'));
+  //       }
+  //     }
+  //     request.open('GET', 'https://data.cityofnewyork.us/resource/uvpi-gqnh.json?$where=health = "Poor"', true);
+  //     request.send();
+  //   });
+  // }
 }
 
 module.exports = new TreeData();
