6af2fc5fe36a76171876db4d2b802f5b3e911542
Matt Waldron <mattwaldron@gmail.com>
2016-06-28 22:56:13 -0400

    Make counters use correct data values and animate on scroll.

--- app.js
+++ app.js
@@ -55,13 +55,31 @@ class GaugeList extends React.Component {
 }
 
 class Gauge extends React.Component {
+  componentWillMount(){
+    this.state = {gaugeFill: 314};
+  }
+
+  _fillGauge(){
+    // Calculate % for gauge
+    const gaugePerimeter = 314;
+    this.setState({gaugeFill: gaugePerimeter - (this.props.data.healthyPercentage * 3.14)}); // 1% of gauge is 3.14  
+  }
   
   render(){
+    let VisibilitySensor = require('react-visibility-sensor');
+
+    let onChange = function(isVisible){
+      if (isVisible) {
+        this._fillGauge();
+      }
+    }.bind(this);
+
     return(
       <li className="gauge">
+        <VisibilitySensor onChange={onChange} />
         <svg class="gauge" width="140" height="140" viewbox="0 0 140 140">
           <circle className="gauge--outer" r="50" cx="75" cy="75"></circle>
-          <circle className="gauge--inner" r="50" cx="75" cy="75" stroke={this.props.data.color}></circle>
+          <circle className="gauge--inner" r="50" cx="75" cy="75" stroke={this.props.data.color} strokeDashoffset={this.state.gaugeFill}></circle>
         </svg>
         <p className="gauge-number">{this.props.data.healthyPercentage}%</p>
         <p>{this.props.data.name}</p>
